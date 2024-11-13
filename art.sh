1696c50f0d214c863dbc60e13bab20ede9142991
AstrologSecra <astrologworkmail@gmail.com>
2024-11-13 17:44:43 +0300

    Create art.sh

---
+++ art.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ASCII Art for KALIBIAN
+print_kalibian_art() {
+    echo "  _  __    _    _     ___ ____ ___    _    _   _ "
+    echo " | |/ /   / \  | |   |_ _| __ )_ _|  / \  | \ | |"
+    echo " | ' /   / _ \ | |    | ||  _ \| |  / _ \ |  \| |"
+    echo " | . \  / ___ \| |___ | || |_) | | / ___ \| |\  |"
+    echo " |_|\_\/_/   \_\_____|___|____/___/_/   \_\_| \_|"
+}
+
+# Print KALIBIAN ASCII Art
+print_kalibian_art
