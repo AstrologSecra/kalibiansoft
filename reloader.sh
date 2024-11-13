9e915359143cd3dc4ce3d97969b21e8bbf43ffcc
AstrologSecra <astrologworkmail@gmail.com>
2024-11-13 09:34:14 +0300

    Update reloader.sh

--- reloader.sh
+++ reloader.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Remove the ossoft directory if it exists
-if [ -d "ossoft" ]; then
-  echo "Removing ossoft directory..."
-  rm -rf ossoft
+# Define the home directory
+HOME_DIR="$HOME"
+
+# Remove the ossoft directory if it exists in the home directory
+if [ -d "$HOME_DIR/ossoft" ]; then
+  echo "Removing ossoft directory from home directory..."
+  rm -rf "$HOME_DIR/ossoft"
   echo "ossoft directory removed."
 else
-  echo "ossoft directory does not exist."
+  echo "ossoft directory does not exist in home directory."
 fi
 
-# Clone the repository
-echo "Cloning repository https://github.com/AstrologSecra/ossoft..."
-git clone https://github.com/AstrologSecra/ossoft
+# Clone the repository into the home directory
+echo "Cloning repository https://github.com/AstrologSecra/ossoft into home directory..."
+git clone https://github.com/AstrologSecra/ossoft "$HOME_DIR/ossoft"
 echo "Repository cloned."
