80aef250b6b04a1c3b5840e6d29bee52eb7dd402
AstrologSecra <astrologworkmail@gmail.com>
2024-11-14 09:42:09 +0300

    Update package-manager.sh

--- edit_system/package-manager.sh
+++ edit_system/package-manager.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
+# Функция для получения списка установленных пакетов
+get_installed_packages() {
+    dpkg-query -W -f='${Package}\n'
+}
+
+# Функция для удаления пакетов
+remove_packages() {
+    packages=("$@")
+    if [ ${#packages[@]} -gt 0 ]; then
+        sudo apt-get remove --purge "${packages[@]}"
+    fi
+}
+
+# Функция для установки пакетов
+install_packages() {
+    packages=("$@")
+    if [ ${#packages[@]} -gt 0 ]; then
+        sudo apt-get install "${packages[@]}"
+    fi
+}
+
 # Функция для получения списка служб
 get_services() {
     systemctl list-units --type=service --state=active --no-pager | awk '{print $1}' | grep -v "UNIT"
-}
-
-# Функция для получения списка файлов в директории
-get_files() {
-    local dir="$1"
-    find "$dir" -type f
 }
 
 # Функция для управления службами
@@ -22,6 +37,12 @@ manage_services() {
     fi
 }
 
+# Функция для получения списка файлов в директории
+get_files() {
+    local dir="$1"
+    find "$dir" -type f
+}
+
 # Функция для удаления файлов
 remove_files() {
     files=("$@")
@@ -32,14 +53,20 @@ remove_files() {
     fi
 }
 
+# Получаем список установленных пакетов
+installed_packages=($(get_installed_packages))
+
 # Получаем список служб
 services=($(get_services))
 
 # Получаем список файлов в /etc
 files=($(get_files "/etc"))
 
-# Создаем список служб и файлов для выбора
+# Создаем список пакетов, служб и файлов для выбора
 item_list=()
+for package in "${installed_packages[@]}"; do
+    item_list+=("$package" "Пакет" "off")
+done
 for service in "${services[@]}"; do
     item_list+=("$service" "Служба" "off")
 done
@@ -71,12 +98,18 @@ action=$(zenity --list --radiolist \
     --text="Что вы хотите сделать с выбранными компонентами?" \
     --column="Выбрать" --column="Действие" \
     TRUE "Остановить службы и удалить файлы" \
+    FALSE "Удалить пакеты" \
+    FALSE "Возвратить пакеты" \
     --width=300 --height=200)
 
 # Выполняем действие
 if [ "$action" == "Остановить службы и удалить файлы" ]; then
     manage_services "${selected_items_array[@]}"
     remove_files "${selected_items_array[@]}"
+elif [ "$action" == "Удалить пакеты" ]; then
+    remove_packages "${selected_items_array[@]}"
+elif [ "$action" == "Возвратить пакеты" ]; then
+    install_packages "${selected_items_array[@]}"
 else
     zenity --info --title="Выход" --text="Действие не выбрано. Выход."
     exit 0
