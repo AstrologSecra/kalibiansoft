db36428768d351137e0e56010514f13bca2e1d14
AstrologSecra <astrologworkmail@gmail.com>
2024-11-16 18:10:54 +0300

    Update package-del.sh

--- edit_system/package-del.sh
+++ edit_system/package-del.sh
@@ -1,20 +1,56 @@
 #!/bin/bash
 
-# Функция для получения списка установленных пакетов
-get_installed_packages() {
+# Функция для получения списка установленных пакетов с использованием apt
+get_installed_packages_apt() {
     dpkg-query -W -f='${Package}\n'
 }
 
-# Функция для удаления пакетов
-remove_packages() {
+# Функция для удаления пакетов с использованием apt
+remove_packages_apt() {
     packages=("$@")
     if [ ${#packages[@]} -gt 0 ]; then
         sudo apt-get remove --purge "${packages[@]}"
     fi
 }
 
+# Функция для получения списка установленных пакетов с использованием pacman
+get_installed_packages_pacman() {
+    pacman -Qq
+}
+
+# Функция для удаления пакетов с использованием pacman
+remove_packages_pacman() {
+    packages=("$@")
+    if [ ${#packages[@]} -gt 0 ]; then
+        sudo pacman -Rns "${packages[@]}"
+    fi
+}
+
+# Функция для выбора пакетного менеджера
+choose_package_manager() {
+    local choice=$(zenity --list --title="Выбор пакетного менеджера" --text="Выберите пакетный менеджер:" --column="Пакетный менеджер" "apt" "pacman")
+
+    case $choice in
+        "apt")
+            get_installed_packages=get_installed_packages_apt
+            remove_packages=remove_packages_apt
+            ;;
+        "pacman")
+            get_installed_packages=get_installed_packages_pacman
+            remove_packages=remove_packages_pacman
+            ;;
+        *)
+            zenity --error --title="Ошибка" --text="Неверный выбор. Выход."
+            exit 1
+            ;;
+    esac
+}
+
+# Выбираем пакетный менеджер
+choose_package_manager
+
 # Получаем список установленных пакетов
-installed_packages=($(get_installed_packages))
+installed_packages=($($get_installed_packages))
 
 # Создаем список пакетов для выбора
 package_list=()
@@ -41,6 +77,6 @@ fi
 IFS=' ' read -r -a selected_packages_array <<< "$selected_packages"
 
 # Удаляем выбранные пакеты
-remove_packages "${selected_packages_array[@]}"
+$remove_packages "${selected_packages_array[@]}"
 
 zenity --info --title="Готово" --text="Операция завершена."
